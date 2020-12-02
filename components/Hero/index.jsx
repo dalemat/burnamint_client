42450243f88642dfe33940ba5e9bbd409b3ab570
dalemat <31269397+dalemat@users.noreply.github.com>
2020-12-02 21:09:10 +0100

    Update index.jsx

--- components/Hero/index.jsx
+++ components/Hero/index.jsx
@@ -1,10 +1,11 @@
-const mainText = "Burn Old Tokens and Get New Tokens Instantly"
-const mainDesc = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Auctor natoque lorem faucibus et posuere non. Massa cras luctus risus, donec aliquet eu, risus. Nibh bibendum viverra ut est."
+const mainText = "Burn Dalc Erc20 Tokens and Get Dfine Tokens. Ratio 10:1"
+const mainDesc = " 70,000 Dfine tokens reserved for the swap."
 export default function Hero({handleConnect}){
     return (
         <div className="flex flex-wrap justify-center items-center md:mt-20">
             <h1 className="text-center text-brownish text-2xl w-300 md:w-710 md:text-5xl font-bold leading-tight">
                 {mainText}
+                
             </h1>
             <p className="text-center text-light-brown md:w-800 text-xl px-4 mt-4 md:mt-4">{mainDesc}</p>
             <div className="flex flex-wrap justify-center w-full my-6 md:my-6">
@@ -12,6 +13,8 @@ export default function Hero({handleConnect}){
             </div>
             <div className="text-light-brown w-full text-center md:text-xl">Connect your wallet to mint</div>
             <button onClick={handleConnect} className="bg-greenish hover:bg-fade-green text-white rounded-md shadow-md hover:shadow-lg h-10 md:h-16 mt-6 px-8 md:px-16 md:mt-8">CONNECT METAMASK</button>
+        
         </div>
     )
+    
 }
